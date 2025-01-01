24b181882bad8d0d3413763e938dea58d8945217
jinnn888 <tuyor.jynchris@gmail.com>
2025-01-01 12:33:06 +0800

    pushing files

---
+++ type-annotations.ts
@@ -0,0 +1,73 @@
+// Variable Annotations
+let name: string = "Hyein";
+let age: number = 17;
+let isActive: boolean = true;
+
+// Function Parameter and Return Type Annotations
+
+function add(a: number, b: number): number {
+	return a + b;
+}
+
+function greet(name: string): string {
+	return `Hello, ${name}`;
+}
+
+function printName(name: string): void {
+	console.log(name)
+}
+
+printName(name)
+console.log(greet(name));
+
+// Array Anotations
+let numbers: number[] = [1, 2, 3, 4];
+let string: string[] = ['a', 'b', 'c', 'd'];
+
+// Object Anotations
+let user: { name: string, age: number } = { name: 'Cat', 2};
+let dog: { name: string, age: number } = { name: 'niji', 2};
+
+// Union types: Allows a variable to have multiple types
+let id: string | number = 22;
+id = 'abc';
+
+// Type aliases
+type User = {name: string, age: number};
+let user1: User = {name: 'idk', age: 22};
+
+// Interface Annotation
+interface Product {
+	id: number;
+	name: string;
+	price: number;
+}
+
+let product: Product = { id: 1, name: 'Laptop', price: 22 };
+
+// Function type annotations
+let calculate: (a: number, b: number) => number;
+calculate = (a, b) => a + b;
+
+// Tuple Annotations
+let tuple: [string, number];
+tuple = ["hello", 42];
+
+// Enum Annotations
+
+enum Status {
+  Active,
+  Inactive,
+  Suspended,
+}
+
+let currentStatus: Status = Status.Active;
+
+// Any Type
+let randomValue: any = 10;
+randomValue = "Hello";
+randomValue = true;
+
+// Type Assertions
+let someValue: unknown = "this is a string";
+let strLength: number = (someValue as string).length;
